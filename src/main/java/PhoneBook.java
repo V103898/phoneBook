119f7b834b7c08832adeca2f1b5f97a0923a5190
CTACOB <star2203@yandex.ru>
2025-05-28 14:05:06 +0300

    add implemented.

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> contacts = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0; // заглушка
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, number);
+        }
+        return contacts.size();
     }
 }
