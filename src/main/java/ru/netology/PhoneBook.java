30018dbf9502c42ab77ec0f28305b6e1ab93cf0e
CTACOB <star2203@yandex.ru>
2025-06-07 15:41:24 +0300

    Stub and tests for add.

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,20 @@
 package ru.netology;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
 
 public class PhoneBook {
-}
+
+    private final Map<String, String> nameToNumber = new HashMap<>();
+    private final Map<String, String> numberToName = new HashMap<>();
+    private final TreeSet<String> sortedNames = new TreeSet<>();
+
+    public int add(String name, String number) {
+        if (!nameToNumber.containsKey(name)) {
+            nameToNumber.put(name, number);
+            numberToName.put(number, name);
+            sortedNames.add(name);
+        }
+        return 0;    }
+
+        }
